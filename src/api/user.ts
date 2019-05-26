9f699ce28ad9a171d0493b8ebe13ff537fbec968
sKawashima <bysk.333@gmail.com>
2019-05-26 18:20:31 +0900

    pushによるデータ保存まで

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,8 +1,28 @@
 import * as express from 'express'
 export const userRouter = express.Router()
+import {UserModel} from '../model/userModel'
 
 userRouter.get('/test', (req, res) => {
   res.json({
     message: 'This is user.ts response'
+  })
+})
+
+userRouter.post('/', (req, res) => {
+  const User = new UserModel()
+
+  User.name = req.query.name
+  User.screen_name = req.query.screen_name
+  User.bio = req.query.bio
+
+  User.save((err) => {
+    if (err) {
+      console.log(err)
+      res.send(err)
+    } else {
+      res.json({
+        message: 'success'
+      })
+    }
   })
 })
